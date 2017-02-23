ff01f637c7e57558874b9a3f2e541548319de312
Pythonese <alexmorson@btinternet.com>
2017-02-23 19:19:10 +0000

    Parser and most of renderer

---
+++ expressionParser.py
@@ -0,0 +1,173 @@
+import math
+
+prefixOperators = {
+    "-": lambda a:-a,
+    "sin": math.sin,
+    "cos": math.cos,
+    "tan": math.tan,
+    "abs": abs,
+    "sqrt": math.sqrt
+}
+
+binaryOperators = {
+    "=": lambda a,b: exec('raise Exception("Not an expression (contains an equals sign)")'),
+    "+": lambda a,b: a+b,
+    "-": lambda a,b: a-b,
+    "*": lambda a,b: a*b,
+    "/": lambda a,b: a/b,
+    "C": lambda n,r: math.factorial(n) / math.factorial(r) / math.factorial(n-r),
+    "^": math.pow
+}
+
+postfixOperators = {
+    "!": math.factorial
+}
+
+class Expression:
+    def __init__(self, expression):
+        self.prefixOperator = ""
+        self.left = ""
+        self.binaryOperator = ""
+        self.right = ""
+        self.postfixOperator = ""
+        
+        expression = expression.replace(" ", "")
+
+        # Check for binary operators
+        for binaryOperator in binaryOperators:
+            depth = 0
+            for index, char in list(enumerate(expression))[::-1]:
+                if char == ")":
+                    depth += 1
+                if char == "(":
+                    depth -= 1
+                    if depth < 0:
+                        raise Exception("Brackets do not match")
+
+                if depth == 0:
+                    if char == binaryOperator:
+                        if not ((index == 0 or expression[index-1] in binaryOperators) and binaryOperator == "-"):
+                            self.operator = binaryOperators[binaryOperator]
+                            self.left = Expression(expression[:index])
+                            self.binaryOperator = binaryOperator
+                            self.right = Expression(expression[index+1:])
+                            return
+
+        # Make sure brackets after prefix operators work as intended when post-multiplying
+        depth = 0
+        for index, char in enumerate(expression):
+            if char == "(":
+                depth += 1
+            if char == ")":
+                depth -= 1
+            if char == ")" and depth == 0:
+                if len(expression)-1 != index:
+                    # Post multiplication
+                    if expression[index+1] in postfixOperators:
+                        if len(expression)-1 != index+1:
+                            # Account for a postfix operator
+                            self.__init__(expression[:index+2]+"*"+expression[index+2:])
+                            return
+                    else:
+                        self.__init__(expression[:index+1]+"*"+expression[index+1:])
+                        return
+        
+        # Check for postfix operators (and implicit multiplication after them)
+        for postfixOperator in postfixOperators:
+            depth = 0
+            for index, char in enumerate(expression):
+                if char == "(":
+                    depth += 1
+                if char == ")":
+                    depth -= 1
+                if char == postfixOperator and depth == 0:
+                    if len(expression)-1 != index:
+                        self.__init__(expression[:index+1]+"*"+expression[index+1:])
+                        return
+                    else:
+                        self.operator = postfixOperators[postfixOperator]
+                        self.postfixOperator = postfixOperator
+                        self.left = Expression(expression[:-len(postfixOperator)])
+                        return
+
+        # Check for prefix operators
+        for prefixOperator in prefixOperators:
+            if expression.startswith(prefixOperator):
+                self.operator = prefixOperators[prefixOperator]
+                self.prefixOperator = prefixOperator
+                self.left = Expression(expression[len(prefixOperator):])
+                return
+
+        # Try to split remainder into implicit multiplications
+        if expression[0].isalpha():
+            if len(expression) > 1:
+                # Starts with letter
+                self.__init__(expression[:1]+"*"+expression[1:])
+                return
+            else:
+                # Just a letter
+                self.left = Constant(expression)
+                return
+        preMultiply = expression[0] != "("
+        depth = 0
+        for index, char in enumerate(expression):
+            if char == "(":
+                depth += 1
+            if char == ")":
+                depth -= 1
+            if preMultiply:
+                if char == "(" or char.isalpha():
+                    # Pre-multiplication or Numerical -> Alpha
+                    self.__init__(expression[:index]+"*"+expression[index:])
+                    return
+            else:
+                if char == ")" and depth == 0:
+                    if len(expression)-1 == index:
+                        # Just wrapped in brackets
+                        self.__init__(expression[1:-1])
+                        return
+                    else:
+                        # Post multiplication
+                        self.__init__(expression[:index+1]+"*"+expression[index+1:])
+                        return
+                    
+        # Left with 'unknown stuff'
+        self.left = Constant(expression)
+
+    def __repr__(self):
+        return f"""Expression:
+ Prefix Operator = {self.prefixOperator}
+            Left = {self.left}
+ Binary Operator = {self.binaryOperator}
+           Right = {self.right}
+Postfix Operator = {self.postfixOperator}"""
+
+    def __str__(self):
+        return f"{self.prefixOperator}({str(self.left)}{self.binaryOperator}{str(self.right)}{self.postfixOperator})"
+
+    def evaluate(self):
+        if self.prefixOperator != "" or self.postfixOperator != "":
+            try:
+                return self.operator(self.left.evaluate())
+            except Exception as e:
+                print(e)
+        elif self.binaryOperator != "":
+            try:
+                return self.operator(self.left.evaluate(), self.right.evaluate())
+            except Exception as e:
+                print(e) 
+        else:
+            return self.left.evaluate()
+
+class Constant:
+    def __init__(self, constant):
+        self.constant = constant
+
+    def __repr__(self):
+        return f"Constant = {self.constant}"
+
+    def __str__(self):
+        return self.constant
+
+    def evaluate(self):
+        return float(self.constant)
---
+++ renderer.py
@@ -0,0 +1,153 @@
+import expressionParser
+from PIL import Image, ImageDraw, ImageFont
+
+font = ImageFont.truetype("courbd.ttf", 16)
+marginTop = 3
+
+def renderText(text):
+    if text == "":
+        return Image.new("RGBA", (1, 1), (255, 255, 255, 255))
+    
+    im = Image.new("RGBA", (1,1))
+    draw = ImageDraw.Draw(im)
+    width, height = draw.textsize(text, font)
+
+    im = Image.new("RGBA", (width, height-marginTop), (255, 255, 255, 255))
+    draw = ImageDraw.Draw(im)
+    draw.text((0, -marginTop), text, (0, 0, 0, 255), font)
+
+    return im
+
+def surroundImageWithText(leftText, image, rightText):
+    imLeft = renderText(leftText)
+    imRight = renderText(rightText)
+
+    height = image.size[1]
+
+    imLeft = imLeft.resize((imLeft.size[0], height))
+    imRight = imRight.resize((imRight.size[0], height))
+
+    return composeImagesHorizontally(imLeft, image, imRight)
+
+def composeImagesHorizontally(*images):
+    width = sum(image.size[0] for image in images)
+    height = max(image.size[1] for image in images)
+
+    im = Image.new("RGBA", (width, height), (255, 255, 255, 255))
+    
+    currentXPos = 0
+    for image in images:
+        im.paste(image, (currentXPos, (height-image.size[1])//2))
+        currentXPos += image.size[0]
+
+    return im
+
+def composeImagesVertically(*images):
+    width = max(image.size[0] for image in images)
+    height = sum(image.size[1] for image in images)
+
+    im = Image.new("RGBA", (width, height), (255, 255, 255, 255))
+    
+    currentYPos = 0
+    for image in images:
+        im.paste(image, ((width-image.size[0])//2, currentYPos))
+        currentYPos += image.size[1]
+
+    return im
+
+def composeImagesDiagonally(centre, tl=None, tr=None, bl=None, br=None):
+    tlw, tlh = (0, 0) if tl is None else tl.size
+    trw, trh = (0, 0) if tr is None else tr.size
+    blw, blh = (0, 0) if bl is None else bl.size
+    brw, brh = (0, 0) if br is None else br.size
+
+    leftWidth = max(tlw, blw)
+    rightWidth = max(trw, brw)
+    topHeight = max(tlh, trh)
+    bottomHeight = max(blh, brh)
+
+    width = leftWidth + centre.size[0] + rightWidth
+    height = topHeight + centre.size[1] + bottomHeight
+
+    im = Image.new("RGBA", (width, height), (255, 255, 255, 255))
+
+    im.paste(centre, (leftWidth, topHeight))
+
+    if tl is not None: im.paste(tl, (leftWidth-tlw, topHeight-tlh))
+    if tr is not None: im.paste(tr, (leftWidth+centre.size[0], topHeight-trh))
+    if bl is not None: im.paste(bl, (leftWidth-blw, topHeight+centre.size[1]))
+    if br is not None: im.paste(br, (leftWidth+centre.size[0], topHeight+centre.size[1]))
+
+    return im
+
+def renderPrefixOperation(operator, left):
+    imLeft = renderExpression(left)
+
+    if operator == "abs":
+        return surroundImageWithText("|", imLeft, "|")
+
+    if operator == "sqrt":
+        height = imLeft.size[1] + 4
+        sqrtTickWidth = max(4, int(height**0.5))
+        width = imLeft.size[0] + sqrtTickWidth + 5 # Random padding
+
+        imSqrt = Image.new("RGBA", (width, height), (255, 255, 255, 255))
+        draw = ImageDraw.Draw(imSqrt)
+        draw.line([(0, height-int(height**0.75)), (sqrtTickWidth, height), (sqrtTickWidth, 0), (width-2, 0), (width-2, int(height**0.5))], (0, 0, 0, 255), 2)
+
+        imSqrt.paste(imLeft, (sqrtTickWidth+2, 4))
+
+        return imSqrt
+
+    # Default
+    return composeImagesHorizontally(renderText(operator), surroundImageWithText("(", imLeft, ")"))
+
+def renderBinaryOperation(left, operator, right):
+    imLeft = renderExpression(left)
+    imRight = renderExpression(right)
+
+    if operator == "*":
+        operator = "×"
+        
+    if operator == "/":
+        divideWidth = max(imLeft.size[0], imRight.size[0]) + 4
+        
+        imDivide = Image.new("RGBA", (divideWidth, 4), (255, 255, 255, 255))
+        draw = ImageDraw.Draw(imDivide)
+        draw.line([(0, 1), (divideWidth, 1)], (0, 0, 0, 255), 2)
+        
+        return composeImagesVertically(imLeft, imDivide, imRight)
+    
+    if operator == "C":
+        return composeImagesDiagonally(renderText("C"), tl=imLeft, br=imRight)
+    
+    if operator == "^":
+        return composeImagesDiagonally(imLeft, tr=imRight)
+
+    # Just render side by side
+    return surroundImageWithText("(", composeImagesHorizontally(imLeft, renderText(operator), imRight), ")")
+
+def renderPostfixOperation(left, operator):
+    imLeft = renderExpression(left)
+    
+    return composeImagesHorizontally(imLeft, renderText(operator))
+
+def renderExpression(expression):
+    if type(expression) == expressionParser.Constant:
+        return renderText(str(expression))
+
+    if expression.prefixOperator != "":
+        return renderPrefixOperation(expression.prefixOperator, expression.left)
+    
+    if expression.binaryOperator != "":
+        return renderBinaryOperation(expression.left, expression.binaryOperator, expression.right)
+
+    if expression.postfixOperator != "":
+        return renderPostfixOperation(expression.left, expression.postfixOperator)
+
+    return renderExpression(expression.left)
+
+if __name__ == "__main__":
+    expression = expressionParser.Expression("sqrt(1+x)/(1-x^2/3!)")
+    im = renderExpression(expression)
+    im.save(f"C:\\Users\\Alex\\Desktop\\TestImages\\Test.png")
